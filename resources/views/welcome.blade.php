ca2550e1c9e09a39954da36120616b7eace39a90
CzarHooijmaijers <czarhooijmaijers772005@gmail.com>
2022-09-20 11:24:43 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <title>Curio site</title>
+    <link rel="stylesheet" href="app.css"> 
+</head>
+
+<body> 
+    <header>
+        <div class="navbar">
+            <div class="wrapper">
+             <img src="img/curio-09-licht-paars-logo-rgb.png" alt="logo" width="auto" height="50px">
+                <nav>
+                    <a href="">Rooster</a>
+                    <a href="">Uitleg vakken</a>
+                    <a href="">Blokken</a>
+                    <a href="">Vernsellen</a>
+                    <a href="">Smoelenboek</a>
+                </nav>
+             </div>
+        </div>
+    </header>
+
+</body>
+ 
