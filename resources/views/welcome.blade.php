05fec72fd46eb20fd4f4c2bb05d41cf1d6d64f75
CzarHooijmaijers <czarhooijmaijers772005@gmail.com>
2023-03-10 11:14:01 +0100

    afbeeldingen toegevoegd en teksten bijgewerkt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,49 +2,124 @@
 <head>
     <title>Curio site</title>
     <link rel="stylesheet" href="app.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro&display=swap" rel="stylesheet">
 </head>
 
 <body> 
     <header>
         <div class="navbar">
             <div class="wrapper">
-             <img src="img/curio-09-licht-paars-logo-rgb.png" alt="logo" width="auto" height="50px">
+             <img src="img/curio-02-wit-logo-rgb.png" alt="logo" width="auto" height="50px">
                 <nav>
-                    <a href="">Rooster</a>
-                    <a href="">Uitleg vakken</a>
-                    <a href="">Blokken</a>
-                    <a href="">Vernsellen</a>
-                    <a href="">Smoelenboek</a>
+                    <a href="#Rooster">Rooster</a>
+                    <a href="#Vakken">Uitleg vakken</a>
+                    <a href="#Benodigdheden">Benodigdheden</a>
+                    <a href="#Blokken_versnellen">Blokken en Vernsellen</a>
+                    <a href="#Smoelenboekje">Smoelenboek</a>
                 </nav>
              </div>
         </div>
     </header>
 
     <main>
-        <div class="container">
+        <div class="rooster_vakken">
 
-            <div class="text">
+            <div class="text" id="Vakken">
                 <p>De vakken die je in het eerste jaar gaat krijgen van de opleiding: Software Developer zijn:
-                Windows development: bij dit vak leer je het programmeren van Windows applicaties met verschillende talen zoals: C#, Python en C++. 
-                Web development: Bij web development ga je je vooral bezig houden met het maken van websites met de taal HTML en PHP, ook moeten deze websites er wel mooi uitzien dus je gaat deze websites ook stijlen met behulp van de taal CSS. Verder wordt er ook nog gewerkt met databases. 
-                Nederlands: Nederlands kun je eigenlijk vergelijken met Nederlands op de middelbare school alleen herhaal je hier grotendeels wat je op de middelbare school al hebt geleerd.
-                Engels: Engels is eigenlijk hetzelfde als Nederlands je herhaalt eigenlijk de theorie die je op de middelbare school al hebt geleerd.
-                Rekenen: Voor rekenen geldt hetzelfde als voor de andere generieke vakken je herhaalt en oefent eigenlijk de theorie die je op de middelbare school hebt gehad.
-                Codelab: Bij Codelab leer de meer de programmeer kant van PHP in plaats van de visuele kant die je gebruikt bij WEB.
-                Praktijk werk: Bij praktijk werk ga je met alle kennis die je hebt opgedaan bij de andere vakken aan de slag aan een grote “Praktijkopdracht”. Hier werk je dan ongeveer 3-4 weken aan.
-                SLB: Tijdens SLB (Studieloopbaan begeleiding) bespreekt je SLB’er de standaard mentorzaken met je klas.
-</p>
+                    Windows development: bij dit vak leer je het programmeren van Windows applicaties met verschillende talen zoals: C#, Python en C++. 
+                    Web development: Bij web development ga je je vooral bezig houden met het maken van websites met de taal HTML en PHP, ook moeten deze websites er wel mooi uitzien dus je gaat deze websites ook stijlen met behulp van de taal CSS. Verder wordt er ook nog gewerkt met databases. 
+                    Nederlands: Nederlands kun je eigenlijk vergelijken met Nederlands op de middelbare school alleen herhaal je hier grotendeels wat je op de middelbare school al hebt geleerd.
+                    Engels: Engels is eigenlijk hetzelfde als Nederlands je herhaalt eigenlijk de theorie die je op de middelbare school al hebt geleerd.
+                    Rekenen: Voor rekenen geldt hetzelfde als voor de andere generieke vakken je herhaalt en oefent eigenlijk de theorie die je op de middelbare school hebt gehad.
+                    Codelab: Bij Codelab leer de meer de programmeer kant van PHP in plaats van de visuele kant die je gebruikt bij WEB.
+                    Praktijk werk: Bij praktijk werk ga je met alle kennis die je hebt opgedaan bij de andere vakken aan de slag aan een grote “Praktijkopdracht”. Hier werk je dan ongeveer 3-4 weken aan.
+                    SLB: Tijdens SLB (Studieloopbaan begeleiding) bespreekt je SLB’er de standaard mentorzaken met je klas.
+                </p>
             </div>
-            <div class="img">
+            <div class="rooster" id="Rooster">
                 <img src="img/rooster.png" alt="rooster" width="auto" height="400px">
             </div>
             
         </div>
-        <div class="bigcontainer">
-            <div class="text2">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente nihil, obcaecati provident esse animi facere voluptatibus. Mollitia voluptatibus culpa similique corrupti necessitatibus earum cupiditate esse iusto quasi. Sed perspiciatis mollitia commodi obcaecati? Natus quos magnam suscipit, architecto iure consectetur eos totam reprehenderit culpa inventore saepe tenetur dolore nisi nostrum vero minus adipisci itaque, commodi veniam aspernatur accusantium quas ut. Aut suscipit fugiat architecto debitis aliquam veritatis ipsa neque! Iste obcaecati dolore aut eaque ullam, velit quae nemo est repudiandae cum ab dicta placeat maxime quaerat architecto facere suscipit deleniti! Placeat natus velit officiis nisi ab ipsam fuga. Temporibus quam quos dolores. Esse numquam, possimus, laborum fugiat tempora illo molestiae alias aperiam minus consequuntur repellendus dolorum perferendis! Doloribus blanditiis, fuga enim deserunt, aut doloremque dolorum aperiam veniam delectus officia quam unde dicta, sint magni totam dolorem esse. Sed, cum porro quo explicabo libero doloremque modi minima adipisci commodi id dignissimos aliquid rem vero asperiores rerum exercitationem, minus et ullam dicta illo tempora temporibus. Aut, delectus vitae. Esse debitis ipsum fuga ex animi dolores nam. Iusto, veniam quas vero iure praesentium vel non, quod veritatis tempore sequi minus maiores! Nostrum nihil commodi ex illum hic, exercitationem fugit alias provident quis consequuntur sed.</p>
+        <div class="benodigdheden" id="Benodigdheden">
+            <div class="text">
+                <p>Elke opleiding heeft wel bepaalde benodigdheden. Deze opleiding heeft de volgende dingen nodig: 
+                    Een laptop: Minimale specificaties van de laptop:
+                    CPU: Intel i5 (10e generatie) / AMD (Ryzen 3000)
+                    Geheugen: 8GB
+                    Opslag: 512GB
+                    Scherm: 15 inch met minimale resolutie van 1080p ('full hd')
+                    Grafische Kaart: geen bijzondere eisen
+                    Besturingssysteem: Windows 10 of hoger
+                    Geïntegreerde webcam   
+                    Licenties voor Nederlands Engels en rekenen: Deze zijn te vinden op de MBOwebshop.
+                    Een multomap:  In de Multomap bewaar je de papieren die je door de jaren heen meekrijgt. 
+                    Een pen en potloden om schetsen te maken voor je toekomstige websites. Denk hierbij bijvoorbeeld aan een wire-frame of een word-web.
+                    Een webcam, microfoon en koptelefoon voor Teams: Deze heb je nodig voor als er een keer een les online gegeven moet worden.
+                </p>
             </div>
         </div>
+        <div class="info">
+            <img src="img/info.jpg" alt="info">
+        </div>
+
+        <div class="blokken_versnellen" id="Blokken_versnellen">
+            <div class="text">
+                <p> De complete opleiding bestaat uit 8 blokken. In leerjaar 1 krijg je blokken 1 en 2. In deze blokken maak je eigenlijk kennis met de opleiding en leer je vooral de basics avn het programmeren.
+                    In het tweede leerjaar heb je de blokken 3 en 4. In blok 3 ga je jezelf eigenlijk voorbereiden op de stage die je gaat doen in blok 4.
+                    je bereidt je voor door je programmeer kennis nog wat uit te breiden. Ook leer je hoe je een cv opbouwd en hoe je zoekt en jezelf aanmeld voor een stage.
+                    In blok 4 ga je ook daadwerkelijk op stage. je moet gemmideld 32 uur in de week. Daarnaa kom je in blok 5 en 6. Deze 2 blokken ga je weer het hele schooljaar naar school om je kennis nog meer uit te breiden.
+                    Dit doe je vooral zodat je in blok 7 en 8, waar je het hele jaar op stage gaat goed voorbereid bent.
+                    Ook zou je deze opleiding sneller kunnen doen. Als je in het eerste blok goed je best hebt gedaan en het makkelijk hebt gehaald dan krijg je de keuze om te Vernsellen.
+                    Dan doe je de opleiding in plaats van in 4 jaar in 3 jaar. Als je dan versnelt dan vervalt dat laatste jaar stage.
+
+                </p>
+            </div>
+        </div>
+
+        <div class="smoelenboekje" id="Smoelenboekje">
+            <div class="personen">
+                <div class="bart">
+                    <p>Bart Roos, Docent Native</p>
+                    <img src="img/bart.jpg" alt="bart">
+                </div>
+                <div class="elton">
+                    <p>Elton Boekhoudt, Docent Web</p>
+                    <img src="img/elton.jpg" alt="elton">
+                </div>
+                <div class="ine">
+                    <p>Ine Vermeer, Docent Web</p>
+                    <img src="img/ine.jpg" alt="ine">
+                </div>
+                <div class="mustafa">
+                    <p>Mustafa Kazar, Docent Engels</p>
+                    <img src="img/kazar.jpg" alt="kazar">
+                </div>
+                <div class="joep">
+                    <p>Joep van Gils, Docent Nederlands</p>
+                    <img src="img/joep.jpg" alt="joep">
+                </div>
+                <div class="vincent">
+                    <p>Vincent Hooft, Docent Codelab</p>
+                    <img src="img/vincent.jpg" alt="vincent">
+                </div>
+                <div class="wiro">
+                    <p>Wiro Verdiesen, Docent Rekenen</p>
+                    <img src="img/wiro.jpg" alt="wiro">
+                </div>
+                <div class="steven">
+                    <p>Steven van Rosendaal, Docent Web</p>
+                    <img src="img/steven.jpg" alt="steven">
+                </div>
+                <div class="fedde">
+                    <p>Fedde van Gils, Docent Native</p>
+                    <img src="img/fedde.jpg" alt="fedde">
+                </div>
+            </div>
+        </div>
+
     </main>
 
 </body>
